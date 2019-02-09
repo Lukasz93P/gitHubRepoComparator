b1f5b2ed9203a431d257c2e35db4302f61b5ba85
Lukasz93P <lukasz9.3@interia.pl>
2019-02-09 10:57:37 +0100

    BasicJsonSerializer with unit tests

---
+++ src/GitHubRepoComparator/Serialization/Serializer/BasicJsonSerializer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace GitHubRepoComparator\Serialization\Serializer;
+
+use GitHubRepoComparator\Serialization\Serializable\Serializable;
+
+/**
+ * Class BasicJsonSerializer
+ * @package Stereotypes\ApiStereotypes\Serializer
+ */
+class BasicJsonSerializer implements Serializer
+{
+    /**
+     * @param Serializable $serializable
+     * @return string
+     */
+    public function serialize(Serializable $serializable)
+    {
+        $serializableFieldsName = $serializable->getSerializableProperties();
+
+        if (empty($serializableFieldsName)) {
+            throw new \RuntimeException(get_class($serializable) . ' has no serializable fields.');
+        }
+
+        $serializableDataArray = array();
+        foreach ($serializableFieldsName as $name) {
+            $getterName = 'get' . $name;
+            $serializableDataArray[$name] = $serializable->$getterName();
+        }
+
+        return json_encode($serializableDataArray);
+    }
+}
