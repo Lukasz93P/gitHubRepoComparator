6b3e669d6bcee76573148bb88fc79aafbfdfe0a8
Lukasz93P <lukasz9.3@interia.pl>
2019-02-09 13:11:31 +0100

    BasicValidator with couple of validation methods with unit tests

---
+++ src/GitHubRepoComparator/Tests/Validation/BasicValidatorTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace GitHubRepoComparator\Tests\Validation;
+
+use GitHubRepoComparator\Validation\BasicValidator;
+
+class BasicValidatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var BasicValidator
+     */
+    private $validator;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->validator = new BasicValidator();
+    }
+
+    public function testShouldNotThrowValidationException()
+    {
+        $data = array('testKey' => 'testValue');
+        $rules = array('testKey' => 'notempty');
+
+        try {
+            $this->validator->validate($rules, $data);
+        } catch (\Exception $exception) {
+            $this->fail('Validation Exception was thrown ' . $exception->getMessage());
+        }
+
+        $this->assertTrue(true);
+    }
+
+    public function testShouldThrowValidationException()
+    {
+        $data = array('testKey' => '', 'secondTestKey' => 'fsdf');
+        $rules = array('testKey' => 'notempty', 'secondTestKey' => 'notempty', 'thirdTestKey' => 'notempty');
+
+        $validationException = false;
+
+        try {
+            $this->validator->validate($rules, $data);
+        } catch (\Exception $exception) {
+            $validationException = $exception;
+        }
+
+        $this->assertNotEmpty($validationException);
+        $this->assertEquals('GitHubRepoComparator\Exception\Validation\ValidationException', get_class($validationException));
+        $this->assertEquals(array('testKey' => array('Cannot be empty'), 'thirdTestKey' => array('Cannot be empty')),
+            $validationException->getValidationErrors());
+    }
+
+    public function testShouldNotThrowValidationExceptionForMaxValue()
+    {
+        $data = array('testKey' => 'testValue', 'secondTestKey' => 'veeeeryLonggggWorddddd');
+        $rules = array('testKey' => 'max:10', 'secondTestKey' => 'max:40');
+
+        try {
+            $this->validator->validate($rules, $data);
+        } catch (\Exception $exception) {
+            $this->fail('Validation Exception was thrown ' . $exception->getMessage());
+        }
+
+        $this->assertTrue(true);
+    }
+
+    public function testShouldThrowValidationExceptionForMaxValue()
+    {
+        $data = array('testKey' => 'testValue', 'secondTestKey' => 'veeeeryLonggggWorddddd');
+        $rules = array('testKey' => 'max:10', 'secondTestKey' => 'max:4');
+
+        $testException = false;
+
+        try {
+            $this->validator->validate($rules, $data);
+        } catch (\Exception $exception) {
+            $testException = $exception;
+        }
+
+        $this->assertNotEmpty($testException);
+        $this->assertEquals(array('secondTestKey' => array('Cannot be greater than 4')), $testException->getValidationErrors());
+    }
+
+    public function testShouldNotThrowValidationExceptionForMinValue()
+    {
+        $data = array('testKey' => 'testValue', 'secondTestKey' => 'veeeeryLonggggWorddddd');
+        $rules = array('testKey' => 'min:1', 'secondTestKey' => 'min:4');
+
+        try {
+            $this->validator->validate($rules, $data);
+        } catch (\Exception $exception) {
+            $this->fail('Validation Exception was thrown ' . $exception->getMessage());
+        }
+
+        $this->assertTrue(true);
+    }
+
+    public function testShouldThrowValidationExceptionForMinValue()
+    {
+        $data = array('testKey' => 'testValue', 'secondTestKey' => 'veeeeryLonggggWorddddd');
+        $rules = array('testKey' => 'min:10', 'secondTestKey' => 'min:4');
+
+        $testException = false;
+
+        try {
+            $this->validator->validate($rules, $data);
+        } catch (\Exception $exception) {
+            $testException = $exception;
+        }
+
+        $this->assertNotEmpty($testException);
+        $this->assertEquals(array('testKey' => array('Cannot be lower/shorter than 10')), $testException->getValidationErrors());
+    }
+}
---
+++ src/GitHubRepoComparator/Validation/BasicValidator.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace GitHubRepoComparator\Validation;
+
+use GitHubRepoComparator\Exception\Validation\ValidationException;
+
+class BasicValidator implements Validator
+{
+    /**
+     * @var array
+     */
+    private $validationErrors;
+
+    public function validate(array $rules, array $data)
+    {
+        foreach ($rules as $validatedPropertyName => $validationRule) {
+            $validationMethodCalls = $this->getValidationMethodsCallsForRule($validationRule);
+            foreach ($validationMethodCalls as $methodName => $methodArgs) {
+                $dataValueForPropertyName = empty($data[$validatedPropertyName]) ? null : $data[$validatedPropertyName];
+                call_user_func_array(array($this, $methodName),
+                    array_merge(array($validatedPropertyName, $dataValueForPropertyName), $methodArgs));
+            }
+        }
+
+        if (!empty($this->validationErrors)) {
+            throw new ValidationException('validation failed', $this->validationErrors);
+        }
+    }
+
+    /**
+     * @param string $rule
+     * @return array
+     */
+    private function getValidationMethodsCallsForRule($rule)
+    {
+        $validationMethodsCalls = array();
+        $ruleValidationConstraints = explode('|', $rule);
+
+        foreach ($ruleValidationConstraints as $validationConstraint) {
+            $constraintData = explode(':', $validationConstraint);
+            $validationMethodsCalls[$constraintData[0]] = array_splice($constraintData, 1);
+        }
+
+        return $validationMethodsCalls;
+    }
+
+    /**
+     * @param string $key
+     * @param mixed|null $value
+     */
+    private function notEmpty($key, $value = null)
+    {
+        if (empty($value)) {
+            $this->validationErrors[$key][] = 'Cannot be empty';
+        }
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @param int $max
+     */
+    private function max($key, $value, $max)
+    {
+        if (empty($value)) {
+            return;
+        }
+
+        $failed = false;
+
+        if (is_numeric($value)) {
+            if ($value > $max) {
+
+                $failed = true;
+            }
+        } elseif (strlen($value) > $max) {
+            $failed = true;
+        }
+
+        if ($failed) {
+            $this->validationErrors[$key][] = 'Cannot be greater than ' . $max;
+        }
+    }
+
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @param int $min
+     */
+    private function min($key, $value, $min)
+    {
+        $failed = false;
+
+        if (empty($value)) {
+            $failed = true;
+        } elseif (is_numeric($value)) {
+            if ($value < $min) {
+
+                $failed = true;
+            }
+        } elseif (strlen($value) < $min) {
+            $failed = true;
+        }
+
+        if ($failed) {
+            $this->validationErrors[$key][] = 'Cannot be lower/shorter than ' . $min;
+        }
+    }
+}
